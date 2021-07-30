f1352d6fd67258a230153b04b542b2105e760b4b
Steven Friend <steven@Stevens-MacBook-Pro.local>
2021-07-30 12:19:21 +0900

    started the what will you do activity

---
+++ scripts/what-will-you-do.js
@@ -0,0 +1,114 @@
+'strict'
+
+const refresh = document.getElementById('refresh');
+const start = document.getElementById('start');
+const circle = document.getElementById('circle');
+const playerB = document.querySelector('.player-B');
+const weekB = document.querySelectorAll('div.day');
+const timer = { 
+  element: document.getElementById('timer'),
+  total: 61 * 1000
+};
+
+let bagB;
+let imagesB;
+let ended = false;
+
+getQuestions();
+
+function getQuestions() {
+  bagB = fillBag();
+  imagesB = getImages();
+  fillWeek(weekB, bagB, imagesB);
+}
+
+addDayEvents(weekB, playerB.querySelector('.sign'));
+
+function addDayEvents(week, sign) {
+  for (let day of week) {
+    addDayAnimation(day);
+  }
+}
+
+function addDayAnimation(day) {
+  const grow = { transform: 'scale(2)' };
+  const shrink = { transform: 'scale(1)' };
+  const forwards = { duration: 300, easing: 'ease-out', fill: 'forwards' };
+  day.addEventListener('click', () => {
+    day.style.zIndex = '2';
+    day.animate(grow, forwards);
+  });
+  day.addEventListener('mouseleave', () => {
+    day.animate(shrink, forwards).onfinish = function() {
+      day.style.zIndex = '0';
+    };
+  });
+}
+
+refresh.addEventListener('click', () => {
+  getQuestions();
+});
+
+start.addEventListener('click', () => {
+  requestAnimationFrame(initializeTimer);
+  start.innerHTML = 'RESET'
+});
+
+function initializeTimer(now) {
+  timer.initial = now;
+  timer.s = 1000;
+  timer.m = timer.s * 60;
+  requestAnimationFrame(runTimer);
+}
+
+function runTimer(now) {
+  const options = {minimumIntegerDigits: 2, useGrouping: false};
+  const runtime = now - timer.initial;
+  const remaining = timer.total - runtime;
+  if(remaining < 0) timesUp();
+  else {
+    const minutes = Math.floor(remaining/timer.m).toLocaleString('en-US', options);
+    const seconds = Math.floor((remaining%timer.m)/timer.s).toLocaleString('en-US', options);
+    const display = `${minutes}:${seconds}`
+    if(timer.display !== display) {
+      timer.element.innerText = display;
+      timer.display = display;
+    }
+    if(!ended) requestAnimationFrame(runTimer);
+  }
+}
+
+function timesUp() { 
+  ended = true;
+  start.innerHTML = 'START';
+}
+
+function fillBag() {
+  const array = [];
+  for (let i=0; i<20; i++) {
+    array[i] = i;
+  }
+  return array;
+}
+
+function getImages() {
+  const array = [];
+  for (let i=0; i<20; i++) {
+    const image = new Image();
+    image.src = `./images/${i}.jpg`;
+    image.className = 'image';
+    array.push(image);
+  }
+  return array;
+}
+
+function fillWeek(week, bag, images) {
+  for (let i=0; i<7; i++) {
+    const pick = Math.floor(Math.random() * bag.length);
+    if (week[i].querySelector('img')) {
+      week[i].replaceChild(images[bag[pick]], week[i].querySelector('img'));
+    };
+    week[i].appendChild(images[bag[pick]]);
+    bag.splice(pick, 1);
+  }
+}
